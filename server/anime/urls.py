7c65b7dd22b309cbc4ede77d85fb420e9ecf07ac
Somron Khabibov <habibov200@gmail.com>
2022-05-19 17:23:57 +0300

    Реализовано поиск и фильтрация по жанрам

--- server/anime/urls.py
+++ server/anime/urls.py
@@ -6,5 +6,8 @@ urlpatterns = [
     path('animelist/', views.AnimeListView.as_view()),
     path('genres/', views.GetAllGenresView.as_view()),
     path('years/', views.GetAllYearsView.as_view()),
-    path('anime-detail/<int:pk>/', views.GetAnimeDetailView.as_view())
+    path('anime-detail/<int:pk>/', views.GetAnimeDetailView.as_view()),
+    path('episode/<int:pk>/', views.GetEpisodeView.as_view()),
+    path('filter/', views.GetAnimeFilterView.as_view()),
+    path('search/', views.SearchAnimeView.as_view())
 ]
--- server/anime/views.py
+++ server/anime/views.py
@@ -1,9 +1,10 @@
 from rest_framework import generics
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from itertools import groupby
 
-from .serializers import AllGenresSerializer, AnimeDetailSerializer, AnimeListSerializer, SeasonYearSerializer
-from .models import Anime, Genre, Season
+from .serializers import AllGenresSerializer, AnimeDetailSerializer, AnimeListSerializer, EpisodeSerializer, SeasonYearSerializer
+from .models import Anime, Episode, Genre, Season
 
 
 # Create your views here.
@@ -45,4 +46,29 @@ class GetAnimeDetailView(APIView):
     def get(self, request, pk):
         anime = Anime.objects.get(id=pk, draft=False)
         serializer = AnimeDetailSerializer(anime)
+        return Response(serializer.data)
+
+
+class GetEpisodeView(APIView):
+    
+    def get(self, request, pk):
+        episode = Episode.objects.get(id=pk)
+        serializer = EpisodeSerializer(episode)
+        return Response(serializer.data)
+
+
+class GetAnimeFilterView(APIView):
+    def get(self, request):
+        if self.request.GET.getlist("genres"):
+            anime = Anime.objects.filter(genres__in=self.request.GET.getlist("genres"), draft=False).distinct()
+        else:
+            anime = Anime.objects.filter(draft=False)
+        serializer = AnimeListSerializer(anime, many=True)
+        return Response(serializer.data)
+
+
+class SearchAnimeView(APIView):
+    def get(self, request):
+        anime = Anime.objects.filter(title__icontains=self.request.GET.get("title"), draft=False)
+        serializer = AnimeListSerializer(anime, many=True)
         return Response(serializer.data)
